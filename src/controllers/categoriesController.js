ce6c69f0b5f549053b7b1d9cdcfeb1293050b50d
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-23 10:02:51 +0200

    HOTFIX aggiunta validazione lunghezza minima nome prodotto, e mostro slug nella index categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -3,7 +3,7 @@ import connection from "../db/connections/connection.js";
 async function index(request, response) {
 
     const query = `
-    select name, description
+    select name, description, slug
     from categories
     `;
 
