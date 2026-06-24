be4af2bd496441693794bb79e5b129d9de80cf36
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-06-24 16:57:59 +0200

    HOTFIX ordinamento per più recenti

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -23,7 +23,7 @@ async function index(request, response) {
         price: "p.price",
         name: "p.name",
         rarity: "p.rarity",
-        created_at: "p.created_at"
+        updated_at: "p.updated_at"
     };
 
     const sortColumn = allowedSorts[sort] || "p.name";
@@ -88,7 +88,7 @@ async function index(request, response) {
             p.rarity,
             p.image,
             p.description,
-            p.created_at
+            p.updated_at
         FROM products p
         ${joinCategory}
         ${whereClause}
